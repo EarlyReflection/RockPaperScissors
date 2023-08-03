cd99450d6a887fcb7a23f8b9924326024b12bc25
Vladimir Dvornikov <earlyreflections@protonmail.com>
2023-08-04 00:08:21 +0300

    added logic and views

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,14 +8,81 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var appChoice = ""
+    @State private var userChoice = ""
+    //    @State private var condition = "win"
+    //    @State private var shouldWin = false
+    @State private var userScore = 0
+    @State private var appScore = 0
+    @State private var round = 1
+    @State private var gameOver = false
+    let items = ["🪨", "📜", "✂️"]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("YOU: \(userScore)")
+            Text("APP: \(appScore)")
+            Text("Round: \(round)")
+            //            Text("You should \(condition)")
+            Spacer()
+            HStack {
+                Spacer()
+                Text(userChoice)
+                    .font(.system(size: 100))
+                Spacer()
+                Text(appChoice)
+                    .font(.system(size: 100))
+                Spacer()
+            }
+            Spacer()
+            HStack {
+                ForEach(0..<3){ number in
+                    Spacer()
+                    Button(items[number]) {
+                        userChoice = items[number]
+                        appChoice = items[Int.random(in: 0..<3)]
+                        checkResult()
+                        round += 1
+                        if round == 11 {
+                            gameOver = true
+                        }
+                    }
+                    .font(.system(size: 50))
+                    .buttonStyle(.bordered)
+                    Spacer()
+                }
+            }
+            .padding(.bottom)
         }
         .padding()
+        .alert("Game Over!", isPresented: $gameOver) {
+            Button("New Game") {
+               resetScore()
+            }
+        }
+    }
+    
+    func resetScore() {
+        userScore = 0
+        appScore = 0
+        round = 1
+        gameOver = false
+    }
+    
+    func checkResult() {
+        if userChoice == items[0] && appChoice == items[1] {
+            appScore += 1
+        } else if userChoice == items[0] && appChoice == items[2] {
+            userScore += 1
+        } else if userChoice == items[1] && appChoice == items[2] {
+            appScore += 1
+        } else if userChoice == items[1] && appChoice == items[0] {
+            userScore += 1
+        } else if userChoice == items[2] && appChoice == items[0] {
+            appScore += 1
+        } else if userChoice == items[2] && appChoice == items[1] {
+            userScore += 1
+        }
     }
 }
 
